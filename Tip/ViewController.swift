7cfb56ca7ce4b403f665b7d5355213a5c3082be5
vikharevam <ViharevAMIP@gmail.com>
2023-04-27 16:27:01 +0300

    Created UI elements and placed constreats

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBAction func inputLabel(_ sender: UITextField) {
+    }
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+
+    @IBAction func tipLabelControl(_ sender: UIStepper) {
+    }
+    
+    @IBAction func percentageValueButton(_ sender: Any) {
+    }
+   
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
 
 }
 
